31acae7a04ddf60270ec4308a831475df05d2c8b
Kyle Isom <kyle@imap.cc>
2025-11-22 01:38:55 -0800

    Add ke

---
+++ Formula/ke.rb
@@ -0,0 +1,23 @@
+class Ke < Formula
+  desc "ke - kyle's editor"
+  homepage "https://git.wntrmute.dev/kyle/ke"
+  head "https://github.com/kisom/ke.git", branch: "main"
+  license "ISC"
+  head "https://github.com/kisom/ke"
+  url "https://github.com/kisom/ke/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 ""
+  version "1.0.2"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build"
+    system "cmake", "--build", "build"
+    bin.install "build/bin/ke"
+  end
+
+  test do
+    # Placeholder test; adjust based on what the project installs (e.g., executable or library)
+    return "true"
+  end
+end
