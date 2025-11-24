510b807254334fc4592b14f7a201534b4cda8864
Kyle Isom <kisom@users.noreply.github.com>
2025-11-24 11:22:13 -0800

    cert 1.5.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/cert.rb
+++ Formula/cert.rb
@@ -6,9 +6,9 @@ class Cert < Formula
   homepage "https://github.com/kisom/cert"
   license "Apache-2.0"
   head "https://github.com/kisom/cert"
-  url "https://github.com/kisom/cert/archive/refs/tags/v1.5.1.tar.gz"
-  sha256 "71b446003f4108d967e6c0c43cc449f20199a90a6472ce0c99f4c5b1f475956e"
-  version "1.5.1"
+  url "https://github.com/kisom/cert/archive/refs/tags/v1.5.2.tar.gz"
+  sha256 "57009fc6315019be76cca188075d4f0100e1d4f74b917e7228e34314c0da8b47"
+  version "1.5.2"
 
   depends_on "go" => :build
 
