7320c9785b8aaf6f6d0b32d4c0d33124da23cdda
Kyle Isom <kisom@users.noreply.github.com>
2025-11-28 03:12:39 -0800

    ke 2.0.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ke.rb
+++ Formula/ke.rb
@@ -4,9 +4,9 @@ class Ke < Formula
   head "https://github.com/kisom/ke.git", branch: "main"
   license "ISC"
   head "https://github.com/kisom/ke"
-  url "https://github.com/kisom/ke/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "eb2ff7835f1246378ab26fe2fc6af6b74befc62f853c8f51ffca117f781b9a2b"
-  version "2.0.0"
+  url "https://github.com/kisom/ke/archive/refs/tags/v2.0.1.tar.gz"
+  sha256 "735da946ea75e1e5f000484c1507eaa2a526556eb2fad050ffb9d36dde8559dd"
+  version "2.0.1"
 
   depends_on "cmake" => :build
 
