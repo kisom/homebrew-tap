dec1472d219f0317c1fefc57e210e1b2221081d7
Kyle Isom <kyle@imap.cc>
2025-11-30 03:24:05 -0800

    New formula: kte

---
+++ Formula/kte.rb
@@ -0,0 +1,29 @@
+class Kte < Formula
+  desc "ke - kyle's editor"
+  homepage "https://git.wntrmute.dev/kyle/kte"
+  head "https://github.com/kisom/kte.git", branch: "main"
+  license "ISC"
+  head "https://github.com/kisom/ke"
+  url "https://github.com/kisom/kte/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 ""
+  version "0.1.0"
+
+  depends_on "cmake" => :build
+  depends_on "sdl2"
+  depends_on "ncurses"
+  depends_on "freetype"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DBUILD_GUI=ON", "-DKTE_USE_PIECE_TABLE=ON"
+    system "cmake", "--build", "build"
+    bin.install "build/kte"
+    bin.install "build/kge"
+
+    man1.install "ke.1"
+  end
+
+  test do
+    # Placeholder test; adjust based on what the project installs (e.g., executable or library)
+    return "true"
+  end
+end
