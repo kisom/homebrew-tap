15b01e0230e0c8a029de269ba330ef7e7c739c51
Kyle Isom <kyle@imap.cc>
2025-11-30 21:56:24 -0800

    update sha256

--- Casks/kge.rb
+++ Casks/kge.rb
@@ -1,6 +1,6 @@
 cask "kge" do
   version "1.0.4"
-  sha256 "c87dbc6aff5411676a471e84905d69c671b62b93b1210bd95c9d776d087de95c"
+  sha256 "7355e463c79af6c0b45b939f34ab9d20f309ce0f29c132354bacce97dece6792"
 
   url "https://github.com/kisom/kte/releases/download/v#{version}/kge.app.zip"
 
