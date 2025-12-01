1e0ad0c1acc85e0337c390ff6b5dd26f60e5b361
Kyle Isom <kisom@users.noreply.github.com>
2025-11-30 20:32:53 -0800

    kte 1.0.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/kte.rb
+++ Formula/kte.rb
@@ -4,9 +4,9 @@ class Kte < Formula
   head "https://github.com/kisom/kte.git", branch: "main"
   license "ISC"
   head "https://github.com/kisom/ke"
-  url "https://github.com/kisom/kte/archive/refs/tags/v1.0.2.tar.gz"
+  url "https://github.com/kisom/kte/archive/refs/tags/v1.0.3.tar.gz"
   sha256 ""
-  version "1.0.2"
+  version "1.0.3"
 
   depends_on "cmake" => :build
   depends_on "sdl2"
