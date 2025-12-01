beec3b80297d8619115a52cfafe91a1bce596879
Kyle Isom <kyle@imap.cc>
2025-11-30 21:59:35 -0800

    fixed release?

--- Casks/kge.rb
+++ Casks/kge.rb
@@ -1,6 +1,6 @@
 cask "kge" do
   version "1.0.4"
-  sha256 "7355e463c79af6c0b45b939f34ab9d20f309ce0f29c132354bacce97dece6792"
+  sha256 "39d0a6d8d46f253d9b2573fee3f1d49599fb28ccb8a7e775941370a16d628e6b"
 
   url "https://github.com/kisom/kte/releases/download/v#{version}/kge.app.zip"
 
