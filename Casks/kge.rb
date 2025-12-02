c92a363743759282a02cb265ad948faf64c57617
Kyle Isom <kyle@imap.cc>
2025-12-02 11:25:11 -0800

    bump kge->1.2.3

--- Casks/kge.rb
+++ Casks/kge.rb
@@ -1,8 +1,8 @@
 cask "kge" do
-  version "1.2.2"
-  sha256 "eb0f667e234b51159234cbcc528ed93bda4f9af8dd3c287255d3fbadf396091c"
+  version "1.2.3"
+  sha256 "512ae2b426e9972581451464758bd9c65a00489ac34402c2fb7e1620bfc40290"
 
-  url "https://github.com/kisom/kte/releases/download/v1.2.2/kge.app.zip"
+  url "https://github.com/kisom/kte/releases/download/v1.2.3/kge.app.zip"
   desc "kyle's text editor"
   homepage "https://github.com/kisom/kte"
 
