e416f676ddbeacc895d7258c9ef188261c108978
Kyle Isom <kyle@imap.cc>
2025-12-03 17:14:14 -0800

    fix string termination

--- Casks/kge.rb
+++ Casks/kge.rb
@@ -1,6 +1,6 @@
 cask "kge" do
   version "1.3.7"
-  sha256 "c6cde7c28a2e0a0b420a043d619bf8a70b72dc9e0fcd085ab46a7c82d9a3a730""
+  sha256 "c6cde7c28a2e0a0b420a043d619bf8a70b72dc9e0fcd085ab46a7c82d9a3a730"
 
   url "https://github.com/kisom/kte/releases/download/v1.3.7/kge.app.zip"
   desc "kyle's text editor"
