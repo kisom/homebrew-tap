df8bd9efa5693ce998bfe32402150c53f9db7551
Kyle Isom <kyle@imap.cc>
2025-12-03 17:13:47 -0800

    kge -> 1.3.7

--- Casks/kge.rb
+++ Casks/kge.rb
@@ -1,8 +1,8 @@
 cask "kge" do
-  version "1.3.6"
-  sha256 "c6cde7c28a2e0a0b420a043d619bf8a70b72dc9e0fcd085ab46a7c82d9a3a730"
+  version "1.3.7"
+  sha256 "c6cde7c28a2e0a0b420a043d619bf8a70b72dc9e0fcd085ab46a7c82d9a3a730""
 
-  url "https://github.com/kisom/kte/releases/download/v1.3.6/kge.app.zip"
+  url "https://github.com/kisom/kte/releases/download/v1.3.7/kge.app.zip"
   desc "kyle's text editor"
   homepage "https://github.com/kisom/kte"
 
