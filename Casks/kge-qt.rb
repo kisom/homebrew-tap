17230002e5f2b0213e559816d418a35de022a935
Kyle Isom <kyle@imap.cc>
2025-12-04 23:40:27 -0800

    naming

--- Casks/kge-qt.rb
+++ Casks/kge-qt.rb
@@ -3,13 +3,13 @@ cask "kge-qt" do
   sha256 "a2a7634ab5832d103dc79cb453bdc1ad63cde754e12bc8e58efc01aa5f6846b3"
 
   url "https://github.com/kisom/kte/releases/download/v1.4.0/kge-qt.app.zip"
-  desc "kyle's text editor"
+  desc "kyle's text editor (Qt build)"
   homepage "https://github.com/kisom/kte"
 
-  app "kge.app"
+  app "kge-qt.app"
   postflight do
     system_command "xattr",
-                   args: ["-r", "-d", "com.apple.quarantine", "#{staged_path}/kge.app"],
+                   args: ["-r", "-d", "com.apple.quarantine", "#{staged_path}/kge-qt.app"],
                    sudo: false
   end
 end
