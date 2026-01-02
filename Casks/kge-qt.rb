03f3f69071213d81935a791c19295001139acdcd
Kyle Isom <kyle@imap.cc>
2026-01-02 09:53:05 -0800

    update hashes

--- Casks/kge-qt.rb
+++ Casks/kge-qt.rb
@@ -1,6 +1,6 @@
 cask "kge-qt" do
   version "1.5.6"
-  sha256 ""
+  sha256 "a2227cc106e5c97ef154db679d891ac5ebee1630433cd6cd3648e315b6f33e47"
 
   url "https://github.com/kisom/kte/releases/download/v1.5.3/kge-qt.app.zip"
   desc "kyle's text editor (Qt build)"
