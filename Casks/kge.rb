1e6eef7f672e85308cfc8588579b1e1383b9821a
Kyle Isom <kyle@imap.cc>
2026-01-02 10:45:04 -0800

    fix version numbers in downloads

--- Casks/kge.rb
+++ Casks/kge.rb
@@ -2,7 +2,7 @@ cask "kge" do
   version "1.5.6"
   sha256 "d6d757864f23a198541e4257d5f5a335a48096e82fd66ab3c0129e6cf86e474e"
 
-  url "https://github.com/kisom/kte/releases/download/v1.5.3/kge.app.zip"
+  url "https://github.com/kisom/kte/releases/download/v1.5.6/kge.app.zip"
   desc "kyle's text editor"
   homepage "https://github.com/kisom/kte"
 
