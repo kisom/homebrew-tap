486687ad76447a43be5170df8fcd00e0ba496187
Kyle Isom <kyle@imap.cc>
2026-01-11 18:35:49 -0800

    remove sha prefix from hashes

--- Casks/kge-qt.rb
+++ Casks/kge-qt.rb
@@ -1,6 +1,6 @@
 cask "kge-qt" do
   version "1.5.8"
-  sha256 "sha256:6d8b8a3096cb335478309eeb124ffdc23c27150c77b0628547241e9d993f9e85"
+  sha256 "6d8b8a3096cb335478309eeb124ffdc23c27150c77b0628547241e9d993f9e85"
 
   url "https://github.com/kisom/kte/releases/download/v1.5.8/kge-qt.app.zip"
   desc "kyle's text editor (Qt build)"
--- Casks/kge.rb
+++ Casks/kge.rb
@@ -1,6 +1,6 @@
 cask "kge" do
   version "1.5.8"
-  sha256 "sha256:0776a6aff49caab4e19fcc788ada035513702c0c99bf5076ae378720cf7c8233"
+  sha256 "0776a6aff49caab4e19fcc788ada035513702c0c99bf5076ae378720cf7c8233"
 
   url "https://github.com/kisom/kte/releases/download/v1.5.8/kge.app.zip"
   desc "kyle's text editor"
