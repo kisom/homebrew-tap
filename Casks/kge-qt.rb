c899163e8775a2a3024c3366418f25c8db98e07b
Kyle Isom <kyle@imap.cc>
2026-01-11 18:26:48 -0800

    update hashes

--- Casks/kge-qt.rb
+++ Casks/kge-qt.rb
@@ -1,8 +1,8 @@
 cask "kge-qt" do
-  version "1.5.6"
-  sha256 "a2227cc106e5c97ef154db679d891ac5ebee1630433cd6cd3648e315b6f33e47"
+  version "1.5.8"
+  sha256 "sha256:6d8b8a3096cb335478309eeb124ffdc23c27150c77b0628547241e9d993f9e85"
 
-  url "https://github.com/kisom/kte/releases/download/v1.5.6/kge-qt.app.zip"
+  url "https://github.com/kisom/kte/releases/download/v1.5.8/kge-qt.app.zip"
   desc "kyle's text editor (Qt build)"
   homepage "https://github.com/kisom/kte"
 
--- Casks/kge.rb
+++ Casks/kge.rb
@@ -1,6 +1,6 @@
 cask "kge" do
   version "1.5.8"
-  sha256 "6d1014e739895a6d92bb4e9d00caeac5102e8061b3e89e6cdb90550c43097e38"
+  sha256 "sha256:0776a6aff49caab4e19fcc788ada035513702c0c99bf5076ae378720cf7c8233"
 
   url "https://github.com/kisom/kte/releases/download/v1.5.8/kge.app.zip"
   desc "kyle's text editor"
