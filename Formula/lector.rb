b128c6dabe1bf95120b1299c93db759f11b8b256
Kyle Isom <kisom@users.noreply.github.com>
2026-03-24 16:32:16 -0700

    lector 1.2.7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/lector.rb
+++ Formula/lector.rb
@@ -2,9 +2,9 @@ class Lector < Formula
   desc "Read-only document viewer for Markdown, reStructuredText, and org-mode"
   homepage "https://github.com/kisom/lector"
   license "MIT"
-  url "https://github.com/kisom/lector/archive/refs/tags/v1.2.6.tar.gz"
-  sha256 "3f1b98121a362b2286636ef0b2fb151bd2d2d6ca98e48ab4427e7b52569fef87"
-  version "1.2.6"
+  url "https://github.com/kisom/lector/archive/refs/tags/v1.2.7.tar.gz"
+  sha256 "fb808a3d4e0416c5f09d54a4692ad9ffce1953e3dc451aba04d12fc741fc43b5"
+  version "1.2.7"
   head "https://github.com/kisom/lector.git", branch: "main"
 
   depends_on "rust" => :build
