f0afb3902ed264575b13ca7888e44949c32e9b90
Kyle Isom <kisom@users.noreply.github.com>
2026-03-25 14:02:46 -0700

    sgard 3.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/sgard.rb
+++ Formula/sgard.rb
@@ -2,9 +2,9 @@ class Sgard < Formula
   desc "Shimmering Clarity Gardener: dotfile management"
   homepage "https://github.com/kisom/sgard"
   head "https://github.com/kisom/sgard"
-  url "https://github.com/kisom/sgard/archive/refs/tags/v3.1.0.tar.gz"
+  url "https://github.com/kisom/sgard/archive/refs/tags/v3.1.1.tar.gz"
   sha256 ""
-  version "3.1.0"
+  version "3.1.1"
 
   depends_on "go" => :build
 
