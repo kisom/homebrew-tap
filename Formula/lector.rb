38cfd8a86a2353070936c00ffa66c66cdc82511d
Kyle Isom <kyle@imap.cc>
2026-08-05 18:28:27 -0700

    lector 1.3.7

--- Formula/lector.rb
+++ Formula/lector.rb
@@ -1,8 +1,8 @@
 class Lector < Formula
   desc "Read-only document viewer for Markdown, reStructuredText, and org-mode"
   homepage "https://github.com/kisom/lector"
-  url "https://github.com/kisom/lector/archive/refs/tags/v1.3.6.tar.gz"
-  sha256 "dc2e09cd9a2370ef4d5985b8ca7f3e9424b0008b9030fea7187d77735b1b3b33"
+  url "https://github.com/kisom/lector/archive/refs/tags/v1.3.7.tar.gz"
+  sha256 "c949d56d12c17d5e02a4ca8fbbda445e94f9d51e9423310e73bcbe3c02a4cdf8"
   license "MIT"
   head "https://github.com/kisom/lector.git", branch: "main"
 
